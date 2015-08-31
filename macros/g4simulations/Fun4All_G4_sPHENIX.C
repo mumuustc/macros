ff8909e7b830357f61304bafb69a79357c5e0c31
Jin Huang <jhuang@bnl.gov>
2015-08-31 11:49:50 -0400

    Use electrons

--- macros/g4simulations/Fun4All_G4_sPHENIX.C
+++ macros/g4simulations/Fun4All_G4_sPHENIX.C
@@ -12,8 +12,8 @@ int Cemc_slats_per_cell = 72; // make it 2*2*2*3*3 so we can try other combinati
 int Cemc_spacal_configuration = -1;
 
 int Fun4All_G4_sPHENIX(
-		       const int nEvents = 100,
-		       const char * inputFile = "e-",
+		       const int nEvents = 2,
+		       const char * inputFile = "mu-",
 		       const char * outputFile = "G4sPHENIXCells.root"
 		       )
 {
