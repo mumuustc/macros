453404065d1ca1a51e1a1f2b0679144304ba7462
Jin Huang <jhuang@bnl.gov>
2015-09-30 12:15:54 -0400

    Max verbosity levels

--- macros/g4simulations/Fun4All_G4_sPHENIX.C
+++ macros/g4simulations/Fun4All_G4_sPHENIX.C
@@ -104,7 +104,7 @@ int Fun4All_G4_sPHENIX(
   //---------------
 
   Fun4AllServer *se = Fun4AllServer::instance();
-  se->Verbosity(0);
+  se->Verbosity(10);
   // just if we set some flags somewhere in this macro
   recoConsts *rc = recoConsts::instance();
   // By default every random number generator uses
