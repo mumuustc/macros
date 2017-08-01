465f006dd69f9c7909853b87a69405bedaca78cf
Jin Huang <jhuang@bnl.gov>
2017-08-01 16:56:36 -0400

    use kaons

--- macros/g4simulations/Fun4All_G4_sPHENIX.C
+++ macros/g4simulations/Fun4All_G4_sPHENIX.C
@@ -1,6 +1,6 @@
 int Fun4All_G4_sPHENIX(
 		       const int nEvents = 10,
-		       const char * inputFile = "/sphenix/data/data02/review_2017-08-02/single_particle/spacal2d/fieldmap/G4Hits_sPHENIX_pi-_eta0_16GeV-0002.root",
+		       const char * inputFile = "/sphenix/data/data02/review_2017-08-02/single_particle/spacal2d/fieldmap/G4Hits_sPHENIX_k-_eta0_16GeV-0002.root",
 		       const char * outputFile = "data/G4sPHENIX.root",
            const char * embed_input_file = "/sphenix/data/data02/review_2017-08-02/sHijing/fm_0-4.list"
 		       )
