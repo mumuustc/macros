0ac6bf175cd24a15d175acb43415c96613463b7e
Jin Huang <jhuang@bnl.gov>
2017-10-17 00:09:58 -0400

    add the second hepmc event

--- macros/g4simulations/Fun4All_G4_sPHENIX.C
+++ macros/g4simulations/Fun4All_G4_sPHENIX.C
@@ -429,7 +429,18 @@ int Fun4All_G4_sPHENIX(
       //! positive ID is the embedded event of interest, e.g. jetty event from pythia
       //! negative IDs are backgrounds, .e.g out of time pile up collisions
       //! Usually, ID = 0 means the primary Au+Au collision background
-      //in->set_embedding_id(2);
+      in->set_embedding_id(1);
+
+      Fun4AllHepMCInputManager *in = new Fun4AllHepMCInputManager( "HepMCInput_2");
+      se->registerInputManager( in );
+      se->fileopen( in->Name().c_str(), inputFile );
+      //in->set_vertex_distribution_width(100e-4,100e-4,30,0);//optional collision smear in space time
+      in->set_vertex_distribution_mean(0,0,1,0);//optional collision central position shift in space time
+      //! embedding ID for the event
+      //! positive ID is the embedded event of interest, e.g. jetty event from pythia
+      //! negative IDs are backgrounds, .e.g out of time pile up collisions
+      //! Usually, ID = 0 means the primary Au+Au collision background
+      in->set_embedding_id(2);
     }
   else
     {
