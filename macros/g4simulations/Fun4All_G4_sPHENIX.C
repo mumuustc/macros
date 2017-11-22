e491f133c12bc95d58ec10b9a40950bf098cc573
Jin Huang <jhuang@bnl.gov>
2017-11-22 12:10:35 -0500

    use 10 cm random vertex

--- macros/g4simulations/Fun4All_G4_sPHENIX.C
+++ macros/g4simulations/Fun4All_G4_sPHENIX.C
@@ -447,7 +447,8 @@ int Fun4All_G4_sPHENIX(
     Fun4AllHepMCInputManager *in = new Fun4AllHepMCInputManager("HepMCInput_1");
     se->registerInputManager(in);
     se->fileopen(in->Name().c_str(), inputFile);
-    //in->set_vertex_distribution_width(100e-4,100e-4,30,0);//optional collision smear in space time
+    in->set_vertex_distribution_width(0,0,10,0);//optional collision smear in space time
+    in->set_vertex_distribution_function(PHHepMCGenHelper::Uniform,PHHepMCGenHelper::Uniform,PHHepMCGenHelper::Uniform,PHHepMCGenHelper::Uniform);
     //in->set_vertex_distribution_mean(0,0,1,0);//optional collision central position shift in space time
     //! embedding ID for the event
     //! positive ID is the embedded event of interest, e.g. jetty event from pythia
