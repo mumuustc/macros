5a105e9ab2abc42c9e19ac289ed76589132b44e1
Jin Huang <jhuang@bnl.gov>
2018-10-19 14:38:50 -0400

    500ev

--- macros/g4simulations/Fun4All_G4_sPHENIX.C
+++ macros/g4simulations/Fun4All_G4_sPHENIX.C
@@ -2,7 +2,7 @@
 using namespace std;
 
 int Fun4All_G4_sPHENIX(
-    const int nEvents = 100,
+    const int nEvents = 500,
     const char *inputFile = "/sphenix/data/data02/review_2017-08-02/single_particle/spacal2d/fieldmap/G4Hits_sPHENIX_e-_eta0_8GeV-0002.root",
     const char *outputFile = "G4sPHENIX.root",
     const char *embed_input_file = "/sphenix/data/data02/review_2017-08-02/sHijing/fm_0-4.list")
