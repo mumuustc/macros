9bea1f361a89a592e9968cf03c49fa86bb80f3b1
Jin Huang <jhuang@bnl.gov>
2019-02-21 14:47:10 -0500

    HIJING update

--- macros/g4simulations/Fun4All_G4_sPHENIX.C
+++ macros/g4simulations/Fun4All_G4_sPHENIX.C
@@ -38,8 +38,8 @@ using namespace std;
 
 
 int Fun4All_G4_sPHENIX(
-    const int nEvents = 100,
-    const char *inputFile = "data/sHijing_0-20fm.dat.bz2",
+    const int nEvents = 2,
+    const char *inputFile = "data/pythia8_200pp_MB.dat",
     const char *outputFile = "G4sPHENIX.root",
     const char *embed_input_file = "/sphenix/data/data02/review_2017-08-02/sHijing/fm_0-4.list")
 {
@@ -514,7 +514,7 @@ int Fun4All_G4_sPHENIX(
     Fun4AllHepMCPileupInputManager *pileup = new Fun4AllHepMCPileupInputManager("HepMCPileupInput");
     se->registerInputManager(pileup);
 
-    const string pileupfile("data/sHijing_0-20fm.dat.bz2");
+    const string pileupfile(inputFile);
     pileup->AddFile(pileupfile);  // HepMC events used in pile up collisions. You can add multiple files, and the file list will be reused.
     //pileup->set_vertex_distribution_width(100e-4,100e-4,30,5);//override collision smear in space time
     //pileup->set_vertex_distribution_mean(0,0,0,0);//override collision central position shift in space time
